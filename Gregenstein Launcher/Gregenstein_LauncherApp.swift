9889b0fb3dfb9aabf9859329e454b062206f85da
gregersundvall <gregersundvall@me.com>
2021-01-22 14:21:28 +0100

    Initial Commit

---
+++ Gregenstein Launcher/Gregenstein_LauncherApp.swift
@@ -0,0 +1,17 @@
+//
+//  Gregenstein_LauncherApp.swift
+//  Gregenstein Launcher
+//
+//  Created by Greger Sundvall on 2021-01-22.
+//
+
+import SwiftUI
+
+@main
+struct Gregenstein_LauncherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
